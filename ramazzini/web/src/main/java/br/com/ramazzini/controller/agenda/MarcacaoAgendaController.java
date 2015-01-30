5febd80a52ccce3a3f4d9d9aadfdc97dfeefa263
pcdias <pedrodias.info@gmail.com>
2015-01-30 08:45:07 -0200

    Trabalhando na agenda...

--- ramazzini/web/src/main/java/br/com/ramazzini/controller/agenda/MarcacaoAgendaController.java
+++ ramazzini/web/src/main/java/br/com/ramazzini/controller/agenda/MarcacaoAgendaController.java
@@ -7,14 +7,11 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ConversationScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.ramazzini.controller.util.AbstractBean;
 import br.com.ramazzini.model.agenda.Agenda;
 import br.com.ramazzini.model.empresa.Empresa;
-import br.com.ramazzini.model.profissional.Profissional;
-import br.com.ramazzini.service.entidade.AgendaService;
 import br.com.ramazzini.util.TimeFactory;
 
 @Named
@@ -25,42 +22,30 @@ public class MarcacaoAgendaController extends AbstractBean implements Serializab
 	
 	private Date dataSelecionada = TimeFactory.createDataHora();
 	
-	private List<Profissional> profissionaisDisponiveis = new ArrayList<Profissional>();
+	private List<Agenda> agendamentos = new ArrayList<Agenda>();
 	
-    @Inject
-    private AgendaService agendaService; 
-    
-    private Profissional profissionalSelecionado;
-    
-    private List<Agenda> agendamentos = new ArrayList<Agenda>();
-    
-    private Agenda agenda;
-    
-    private Empresa empresaSelecionada;
-    
+	private Agenda agenda;
+	
+	private Empresa empresaSelecionada;
+	    
 	@PostConstruct
 	public void init() {
 		beginConversation();
 	}    
-    
-    public void editarAgendamento(Agenda agenda) {
-    	this.agenda = agenda;
-   		empresaSelecionada = (agenda.getFuncionario() != null) ? agenda.getFuncionario().getEmpresa() : null;
-    }
-    
-    public void gravarMarcacao() {
-    	agendaService.salvar(agenda);
-    }
 	
-	public void onChangeDataSelecionada() {
-		profissionaisDisponiveis.clear();
-		agendamentos.clear();
+	public void incluirAgendamento() {
+		agenda = new Agenda();
+		agenda.setDataAgenda(dataSelecionada);
 	}
 	
-	public void onChangeProfissionalSelecionado() {
-		agendamentos.clear();
+	public void gravarAgendamento() {
+		
 	}
 	
+	public List<Agenda> getAgendamentos() {
+		return agendamentos;
+	}	
+
 	public Date getDataSelecionada() {
 		return dataSelecionada;
 	}
@@ -69,49 +54,8 @@ public class MarcacaoAgendaController extends AbstractBean implements Serializab
 		this.dataSelecionada = dataSelecionada;
 	}
 
-	public String getCabecalhoCentro() {
-		
-		String cabecalho = "Data: " + TimeFactory.converterDataEmTexto(dataSelecionada);
-		cabecalho = cabecalho + " :: " + TimeFactory.diaDaSemana(dataSelecionada).getStringChave();
-		
-		if (profissionalSelecionado != null) {
-			cabecalho = cabecalho + " | Profissional: " + profissionalSelecionado.getNome();
-		}
-		
-		return cabecalho;
-	}
-	
-	public List<Profissional> getProfissionaisDisponiveis() {
-		if (profissionaisDisponiveis.isEmpty()) {
-			profissionaisDisponiveis = agendaService.recuperarProfissionaisDisponiveisPorData(dataSelecionada);
-		}
-		if (profissionaisDisponiveis.size() > 0) {
-			profissionalSelecionado = profissionaisDisponiveis.get(0);
-		}		
-		return profissionaisDisponiveis;
-	}
-
-	public void setProfissionaisDisponiveis(List<Profissional> profissionaisDisponiveis) {
-		this.profissionaisDisponiveis = profissionaisDisponiveis;
-	}
-
-	public Profissional getProfissionalSelecionado() {
-		return profissionalSelecionado;
-	}
-
-	public void setProfissionalSelecionado(Profissional profissionalSelecionado) {
-		this.profissionalSelecionado = profissionalSelecionado;
-	}
-
-	public List<Agenda> getAgendamentos() {
-		if (agendamentos.isEmpty()) {
-			agendamentos = agendaService.recuperarPorDataProfissional(dataSelecionada, profissionalSelecionado);
-		}
-		return agendamentos;
-	}
-
-	public void setAgendamentos(List<Agenda> agendamentos) {
-		this.agendamentos = agendamentos;
+	public String getDiaSemana() {
+		return TimeFactory.diaDaSemana(dataSelecionada).getStringChave();
 	}
 
 	public Agenda getAgenda() {
@@ -129,9 +73,6 @@ public class MarcacaoAgendaController extends AbstractBean implements Serializab
 	public void setEmpresaSelecionada(Empresa empresaSelecionada) {
 		this.empresaSelecionada = empresaSelecionada;
 	}
-
-	public String getDiaSemana() {
-		return TimeFactory.diaDaSemana(dataSelecionada).getStringChave();
-	}
+	
 	
 }
